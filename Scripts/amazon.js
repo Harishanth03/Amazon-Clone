78baec1663e26ae7202e08c876fc847da9ef7760
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-10 09:57:40 +0530

    Add the Product using Modules

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -1,52 +1,4 @@
-// first we need to save the data  into javascript so i created a array and into the array i created object to save the details.
-const products = [
-
-    {
-        images : '../images/products/athletic-cotton-socks-6-pairs.jpg',
-
-        name : 'lack and Gray Athletic Cotton Socks - 6 Pairs',
-
-        rating : {
-            
-            stars : 4.5,
-
-            count : 87
-        },
-
-        priceInCent : 1090
-    },
-
-    {
-        images : '../images/products/intermediate-composite-basketball.jpg',
-
-        name : 'Intermediate Size Basketball',
-
-        rating : {
-            
-            stars : 4,
-
-            count : 127
-        },
-
-        priceInCent : 2095
-    },
-
-    {
-        images : '../images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-
-        name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-
-        rating : {
-            
-            stars : 4.5,
-
-            count : 65
-        },
-
-        priceInCent : 799
-    },
-
-];
+import { products} from "../data/products.js";
 
 //============================ We need to create a loop throw an array for genegrate the HTML for every product ======================
 
@@ -54,30 +6,30 @@ const products = [
 
 let productHtml = ''; // its initialize outside the loop because everytime when loop works its should be empty string
 
-products.forEach(product => {
+products.forEach(products => {
 
     // += acually every time loops works the HTMl append to the existing html for create a list of product
     productHtml += `
         <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${product.images}">
+              src="${products.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            ${product.name}
+            ${products.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
+              src="images/ratings/rating-${products.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.count}
+              ${products.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceInCent / 100).toFixed(2)}
+            $${(products.priceInCent / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
