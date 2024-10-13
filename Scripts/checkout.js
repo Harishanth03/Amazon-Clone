b457b65a2d9d4f8bc2dd230d1081f4cd3dcc22cc
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-13 08:39:47 +0530

    set dayjs as default export

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -4,6 +4,8 @@ import {products} from '../data/products.js';
 
 import { formatCurrency } from './utils/money.js';
 
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+
 //=================================================== Define Today Date =============================================================
 
 const today = dayjs();
