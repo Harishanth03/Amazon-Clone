cbb2252ec729c711f5e955517d3d9800b7c11623
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-16 23:09:17 +0530

    finalize Today

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -5,7 +5,6 @@ import { rendorOrderSummery } from "../Scripts/CheckOut/orderSummary.js";
 
 import { renderPaymentSummary } from "./CheckOut/paymentSummary.js";
 
-//import '../data/cart-class.js';
 
 
 
