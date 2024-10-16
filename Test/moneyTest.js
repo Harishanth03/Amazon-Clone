89fb86a3b86520a44f55633eb5d5b4ddb1cc98de
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-16 08:40:39 +0530

    set jasmine framwork for testing

--- Test/moneyTest.js
+++ Test/moneyTest.js
@@ -1,5 +1,9 @@
 import {formatCurrency} from '../Scripts/utils/money.js';
 
+console.log('test suits: formatCurrency');
+
+console.log('converts cents into dollars')
+
 if(formatCurrency(2095) === '20.95')
 {
     console.log('Passed');
