b1b34330b91d4cf3973ce7ef46bd05de617dfec7
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-16 12:47:42 +0530

    learn OOP

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -5,6 +5,8 @@ import { rendorOrderSummery } from "../Scripts/CheckOut/orderSummary.js";
 
 import { renderPaymentSummary } from "./CheckOut/paymentSummary.js";
 
+import '../data/cart-OOP.js';
+
 
 
 
