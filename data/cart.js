216e980e9510d8bfb9f502bc3a9d92535a68e074
Harishanth03 <kandeepanharishanth@gmail.com>
2024-10-16 12:12:17 +0530

    create automated testing for add cart

--- data/cart.js
+++ data/cart.js
@@ -1,28 +1,35 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart)
+loadLocalStorage();
+
+export function loadLocalStorage()
 {
-  cart = [
+  cart = JSON.parse(localStorage.getItem('cart'));
 
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  
-      quantity: 2,
+  if(!cart)
+  {
+    cart = [
 
-      deliveryOptionId : '1'
-    },
-  
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  
-      quantity: 1,
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    
+        quantity: 2,
 
-      deliveryOptionId : '2'
+        deliveryOptionId : '1'
+      },
+    
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    
+        quantity: 1,
 
-    }
-  
-  ];
-};
+        deliveryOptionId : '2'
+
+      }
+    
+    ];
+  };  
+}
 
 function saveToStorage()
 {
